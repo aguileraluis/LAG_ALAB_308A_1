18deb4db6ae0199ab51cb2b0d0ed7b72aae54531
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-10-22 19:32:58 -0700

    finished

--- index.js
+++ index.js
@@ -1,15 +1,84 @@
-let count = 0; 
+// Part One: Stack Overflow
+let count = 0;
 
 try {
   function increment() {
-    count++;  
-    increment(); 
+    count++;
+    increment();
   }
-  increment(); 
- 
+  increment();
+
 } catch (error) {
-  console.log(error); 
-  console.log(count); 
+  console.log(error);
+  console.log(count);
 }
+// Part Two: Trampolines
+
+// Step One
+const flatFunctionOriginal = (array) => { 
+  const result = []; 
+
+  array.forEach((arrayItem) => {
+    if (Array.isArray(arrayItem)) {
+      result.push(...flatFunctionOriginal(arrayItem)); 
+    } else {
+      result.push(arrayItem); 
+    }
+  }); 
+
+  return result; 
+};
+
+console.log(flatFunctionOriginal([
+  [ ["hello", "how", "is", "this", "working"],
+  ["is", "this", "working"],
+  ["is", "it", "working"]], 
+  [ ["hello2", "how2", "is2", "this2", "working2"],
+  ["is2", "this2", "working2"],
+  ["is2", "it2", "working2"]],
+ ,
+])); 
+
+
+
+// Step Two
+const flatFunction = (array, result = []) => { 
+
+  if (Array.isArray(array)) {
+  array.forEach((arrayItem) => {
+    
+    result = result.concat(arrayItem); 
+  
+  })   } else {
+    return;
+  }
+
+  console.log(result.flat()); 
+  return () => flatFunction(array[0] + 1); 
+};
+
+// Step Three
+const trampoline = fn => (...args) => {
+  let result = fn(...args)
+  while (typeof result === 'function') {
+    result = result()
+  }
+  return result
+}
+
+const flatArray = trampoline(flatFunction); 
+
+(flatArray([
+  [ ["hello", "how", "is", "this", "working"],
+  ["is", "this", "working"],
+  ["is", "it", "working"]], 
+  [ ["hello2", "how2", "is2", "this2", "working2"],
+  ["is2", "this2", "working2"],
+  ["is2", "it2", "working2"]],
+]));
+
+// Part Three is in partThree.js file.
+
+
 
 
